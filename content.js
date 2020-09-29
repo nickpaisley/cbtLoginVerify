8a55b13eef67e751c35d045e143786d36c124e98
Nicholas Paisley <nicholas.paisley@sb-tn-t6a2jg5j.local>
2020-09-29 15:09:32 -0500

    cleaning up dead space in content.js

--- content.js
+++ content.js
@@ -1,10 +1,7 @@
-// Let's search the page for our logged in email address
-var emailCheck = (document.getElementById('loggedInEmail'));
 
 // First set a timeout to allow the page to load, then check emailCheck if it contains our email. 
 // If so, do nothing..If not, set background.
 setTimeout(() => {
-
         var emailCheck = (document.getElementById('loggedInEmail'));
         var loggedInEmail = (emailCheck.innerText);
         console.log(loggedInEmail);
