fa73a1f9b33827e5283cba38ab80f44a575eb1ad
Nicholas Paisley <nicholas.paisley@sb-tn-t6a2jg5j.local>
2020-09-29 15:38:57 -0500

    cleaning up dead space in content.js

--- content.js
+++ content.js
@@ -2,17 +2,17 @@
 // First set a timeout to allow the page to load, then check emailCheck if it contains our email. 
 // If so, do nothing..If not, set background.
 setTimeout(() => {
-        var emailCheck = (document.getElementById('loggedInEmail'));
-        var loggedInEmail = (emailCheck.innerText);
-        console.log(loggedInEmail);
-        
-        if (loggedInEmail.includes('nicholas.paisley@smartbear.com')) {
-          console.log('yep');
-        }
-        else {
-          console.log('nope');
-          document.body.style.backgroundColor = "#fcdb03";
-        }
+    var emailCheck = (document.getElementById('loggedInEmail'));
+    var loggedInEmail = (emailCheck.innerText);
+    console.log(loggedInEmail);
+    
+    if (loggedInEmail.includes('nicholas.paisley@smartbear.com')) {
+      console.log('yep');
+    }
+    else {
+      console.log('nope');
+      document.body.style.backgroundColor = "#fcdb03";
+    }
 }, 2500)
 
 
